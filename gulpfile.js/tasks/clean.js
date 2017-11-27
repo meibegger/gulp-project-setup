1a410e33ccb03ade1a03d18e8eaf7a46cd2c02a2
Mandana Eibegger <me@schoener.at>
2017-11-27 23:12:19 +0100

    force completion of the clean task before continuing

--- gulpfile.js/tasks/clean.js
+++ gulpfile.js/tasks/clean.js
@@ -2,6 +2,4 @@ const gulp = require('gulp');
 const del = require('del');
 const cleanSpec = require('../config.json').options.clean;
 
-gulp.task('clean', () =>
-  cleanSpec.forEach(path => del(path))
-);
+gulp.task('clean', () => del(cleanSpec));
